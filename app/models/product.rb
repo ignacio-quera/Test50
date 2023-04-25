f6190564a5a476d07c3d111d6044f7d092d4029e
vavengh <vavendanogh@uc.cl>
2023-04-25 17:30:25 -0400

    productos con imagenes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
     has_many :reviews
     accepts_nested_attributes_for :reviews
+    has_one_attached :image
     validates :name, :category, :price, :description, :image, presence: true
     validates :price, numericality: { greater_than_or_equal_to: 0 }
     validates :category, inclusion: { in: ["CR7", "Leo", "Ney"] }
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   # devise_for :users
+  resources :products
   resources :products, only: [:new]
   resources :products, only: [:index, :show] do
     get 'filter_by_category/:category', action: :filter_by_category, on: :collection
