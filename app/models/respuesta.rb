55fe80cf04beaa81c5b68599601df01aefdebe2a
vavengh <vavendanogh@uc.cl>
2023-04-26 17:46:15 -0400

    Foro completo

--- app/models/respuesta.rb
+++
@@ -1,3 +0,0 @@
-class Respuesta < ApplicationRecord
-  belongs_to :pregunta
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   delete '/products/:product_id/reviews/:id', to: 'reviews#destroy', as: :delete_product_review
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
-  # devise_for :users
+  resources :questions do
+    resources :answers, only: [:new, :create, :destroy]
+  end
   resources :products
-  resources :products, only: [:new]
-  resources :products do
+  resources :products, only: [:new] do
     resources :reviews, only: [:create, :destroy]
   end
   resources :reviews do
@@ -13,12 +14,7 @@ Rails.application.routes.draw do
   resources :products, only: [:index, :show] do
     get 'filter_by_category/:category', action: :filter_by_category, on: :collection
   end
-  resources :products, only: [:index, :new, :create, :edit, :update, :destroy]
   root to: 'render#index'
   patch '/products/:id', to: 'products#update'
   get 'render/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
